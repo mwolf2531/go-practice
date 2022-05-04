cce4528d7a1c83534c8d94d312ee2b84c4a05f15
Megan Wolf <megan.wolf@outreach.io>
2022-05-03 21:48:48 -0400

    Finished for the night

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -7,29 +7,27 @@ import (
 )
 
 func main() {
-	// csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question, answer'")
-	// flag.Parse()
 
 	file, err := os.Open("quiz_game/problems.csv")
 
 	if err != nil {
-		fmt.Println("Cannot read file 1")
+		fmt.Println("Cannot read file")
 	}
 
 	csvReader := csv.NewReader(file)
 
-	//lines, err := csvReader.ReadAll()
-
 	if err != nil {
-		fmt.Println("Cannot read file 2")
+		fmt.Println("Cannot read file")
 	}
+
+	//correct := 0
 
 	for {
 		record, err := csvReader.Read()
 		if err != nil {
 			break
 		}
-		fmt.Printf("Question: %s Answer %s\n", record[0], record[1])
+		fmt.Printf("Problem #%s Answer %s\n", record[0], record[1])
 	}
 
 }
