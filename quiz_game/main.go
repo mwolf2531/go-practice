56cecad360ee1723de6b68109cc1c4a1b9470e2d
Megan Wolf <megan.wolf@outreach.io>
2022-05-12 15:02:32 -0400

    Problems print out

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -15,19 +15,36 @@ func main() {
 	}
 
 	csvReader := csv.NewReader(file)
+	lines, err := csvReader.ReadAll()
 
 	if err != nil {
 		fmt.Println("Cannot read file")
 	}
 
-	//correct := 0
-
-	for {
-		record, err := csvReader.Read()
-		if err != nil {
-			break
-		}
-		fmt.Printf("Problem #%s Answer %s\n", record[0], record[1])
-	}
-
+	problems := parseLines(lines)
+	fmt.Println(problems)
 }
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+// for {
+// 	record, err := csvReader.Read()
+// 	if err != nil {
+// 		break
+// 	}
+// 	fmt.Printf("Problem #%s Answer %s\n", record[0], record[1])
+// }
